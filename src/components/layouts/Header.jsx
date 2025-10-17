dd92b28c0280a9cf1cd92f7de297ebb4f367eec6
motasimdev <themotasimdev@gmail.com>
2025-10-17 19:24:36 +0600

    header

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,32 +1,35 @@
 import React from "react";
 import { Link } from "react-router";
-import { PiChatCircleDotsDuotone } from "react-icons/pi";
+import { RiChatAiFill } from "react-icons/ri";
 
 const Header = () => {
   return (
     <>
-      <div className="py-10 bg-gray-200">
+      <div className="py-5 bg-gray-200">
         <div className="max-w-[1320px] m-auto">
-          <div className="flex items-center ">
-            <div className=""></div>
+          <div className="flex items-center justify-between">
+            <div className="flex flex-col items-center">
+              <RiChatAiFill className="text-2xl"/>
+              <p className="text-lg text-black">chatty</p>
+            </div>
             <div className="">
-               <ul className="flex items-center justify-end gap-x-4">
-            <Link to={"/"}>
-              <li className="hover:text-[#916363] text-amber-950 text-md">
-                Home
-              </li>
-            </Link>
-            <Link to={"/signup"}>
-              <li className="hover:text-[#916363] text-amber-950 text-md">
-                SignUp
-              </li>
-            </Link>
-            <Link to={"/login"}>
-              <li className="hover:text-[#916363] text-amber-950 text-md">
-                LogIn
-              </li>
-            </Link>
-          </ul>
+              <ul className="flex items-center justify-end gap-x-4">
+                <Link to={"/"}>
+                  <li className="hover:text-[#916363] text-amber-950 text-md">
+                    Home
+                  </li>
+                </Link>
+                <Link to={"/signup"}>
+                  <li className="hover:text-[#916363] text-amber-950 text-md">
+                    SignUp
+                  </li>
+                </Link>
+                <Link to={"/login"}>
+                  <li className="hover:text-[#916363] text-amber-950 text-md">
+                    LogIn
+                  </li>
+                </Link>
+              </ul>
             </div>
           </div>
         </div>
